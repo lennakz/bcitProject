1fa2a4d08ae1d3728408764d107ef258ab6869e7
Mykola Kotok <mykola.kotok@gmail.com>
2017-03-23 14:24:28 -0700

    Fixed content and links on global nav

--- partials/globalNavigation.php
+++ partials/globalNavigation.php
@@ -1,11 +1,10 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="/"><img class="img-responsive" src="images/bcit-logo.png" alt="BCIT Logo" width="40"></a>
+      <a class="navbar-brand" href="#"><img class="img-responsive" src="images/bcit-logo.png" alt="BCIT Logo" width="40"></a>
     </div>
     <div class="global-links">
       <ul class="nav navbar-nav">
-<<<<<<< HEAD
         <li><a href="about.php">About</a></li>
         <li><a href="lectures.php">Lectures</a></li>
         <li><a href="resources.php">Resources</a></li>
@@ -22,36 +21,6 @@
           </ul>
         </li>
       </ul>
-=======
-        <!--  REMOVED GLOBAL LINKS, REPLACED WITH BCITCOMP1950 RELATED LINKS
-        <li><a href="http://http://www.bcit.ca/">BCIT</a></li>
-        <li><a href="https://learn.bcit.ca/">D2L</a></li>
-        <li><a href="http://www.bcit.ca/mybcit/login/">myBCIT</a></li>
-        <li><a href="http://www.bcit.ca/about/locations.shtml">Campuses</a></li>
-        <li><a href="http://www.bcit.ca/contacts/">Contacts</a></li>
-        <li><a href="http://www.bcit.ca/calendar/">Calendar</a></li>
-      </ul>
-      -->
-        
-        <li><a href="../about.php">About This Course</a></li>
-        <li><a href="../lectures.php">Lectures</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="http://bcitcomp.ca/students/ddimchuk/homepage/project/meta/project-introduction.html">Meta Webpage</a></li>
-        
-        
-      <!-- COMMENTED OUT SEARCH BAR
-      <form class="navbar-form navbar-left">
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="Search">
-          <div class="input-group-btn">
-            <button class="btn btn-default" type="submit">
-              <i class="glyphicon glyphicon-search"></i>
-            </button>
-          </div>
-        </div>
-      </form>
-      -->
->>>>>>> origin/master
     </div>
 
     <ul class="nav navbar-nav navbar-right">
