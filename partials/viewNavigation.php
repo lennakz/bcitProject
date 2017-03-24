e4a2615e1014e857c9991fbec0619e731ec38697
DariaDimchuk <daria.dimchuk@gmail.com>
2017-03-24 14:08:26 -0700

    Added print button function

--- partials/viewNavigation.php
+++ partials/viewNavigation.php
@@ -1,8 +1,14 @@
+<script>
+function triggerPrint() {
+    window.print();
+}
+</script>
+
 <nav class="navbar">
   <div class="container-fluid">
     <div class="global-links">
       <ul id="change-view-nav" class="nav navbar-nav">
-        <li><a id="print" href="#"><i class="fa fa-print"></i> Print Lecture</a></li>
+        <li><button id="print-button"  onclick="triggerPrint()"><a id="print-link" href="#"><i class="fa fa-print"></i> Print Lecture</a></button></li>
         <li><a id="projector" href="#"><i class="fa fa-square-o" aria-hidden="true"></i> Projector View</a></li>
       </ul>
     </div>
