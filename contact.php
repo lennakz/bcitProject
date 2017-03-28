4ff17b8783e5a52ce4d6003b87504e67deabf770
Mykola Kotok <mykola.kotok@gmail.com>
2017-03-27 23:47:37 -0700

    Contact form fix

--- contact.php
+++ contact.php
@@ -25,23 +25,29 @@
           <div class="container">
             <p class="text-center">If you have any question you can contact your teacher using phone/email or you can fill out this form.</p>
             <br><br>
-            <form class="form-horizontal">
+            <form class="form-horizontal" action="php/contact.php">
               <div class="form-group">
-                <label class="control-label col-sm-2" for="email">Name:</label>
+                <label class="control-label col-sm-2" for="name">Name:</label>
                 <div class="col-sm-10">
-                  <input type="text" class="form-control" id="name" placeholder="Enter your name">
+                  <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
                 </div>
               </div>
               <div class="form-group">
-                <label class="control-label col-sm-2" for="pwd">Student #:</label>
+                <label class="control-label col-sm-2" for="student_number">Student #:</label>
                 <div class="col-sm-10">
-                  <input type="text" class="form-control" id="student_number" placeholder="Enter your student number">
+                  <input type="text" class="form-control" id="student_number" name="student_number" placeholder="Enter your student number">
                 </div>
               </div>
               <div class="form-group">
-                <label class="control-label col-sm-2" for="pwd">Question:</label>
+                <label class="control-label col-sm-2" for="email">Email:</label>
                 <div class="col-sm-10">
-                  <input type="text" class="form-control" id="student_number" placeholder="Enter your questions/comments">
+                  <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="control-label col-sm-2" for="question">Question:</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="question" name="question" placeholder="Enter your questions/comments">
                 </div>
               </div>
               <div class="form-group">
